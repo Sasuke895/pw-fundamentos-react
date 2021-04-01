85c826726beb86f4372d016278d3019e7d35b18c
Sasuke895 <lendadoscampeao@hotmail.com>
2021-04-01 13:31:38 +0000

    Aula02 - Fundamentos02
    Signed-off-by: Sasuke895 <lendadoscampeao@hotmail.com>

---
+++ fundamentos-react/src/components/basico/OlaMundo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default () => {
+
+	const informacao = 'Primeiro componente JSX'
+
+	return(
+
+		<>
+			<h2>Componenete OlaMundo</h2>
+			<p>{ informacao }</p>
+            
+		</>
+	 )
+}
